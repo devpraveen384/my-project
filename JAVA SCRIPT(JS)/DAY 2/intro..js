8f88e0a4b9fb4ba1156bbe1ba76982cf214c50f6
devpraveen384 <dev.praveen384@gmail.com>
2026-05-12 22:47:21 +0530

    push

--- JAVA SCRIPT(JS)/DAY 2/intro..js
+++ JAVA SCRIPT(JS)/DAY 2/intro..js
@@ -23,4 +23,42 @@
 
    </script>
 </body>
+
 </html>
+
+<script>
+
+    console.log('Before init ',userName);
+
+    var userName = "React"
+
+    console.log('Init',userName)
+
+    var userName = "Node"
+    console.log('Re Declarate',userName);
+    userName = "JS"
+
+
+    let userEmail  = "react@gmail.com"
+    
+    console.log(userEmail);
+    
+    userEmail = "react12@gmail.com"
+   console.log(userEmail);
+
+
+
+   const a=10; 
+
+
+   const emptyValue = null
+    console.log(typeof emptyValue);
+    
+
+    const newNumber = parseInt(10)
+    const newString = parseInt("ten")
+    
+    console.log(newNumber+newString);
+    
+    
+</script>
